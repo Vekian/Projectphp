953e173fd4686f33671ab047e1d63934dda4a9a2
Mathieu Gerard <mathieu2991@gmail.com>
2023-07-17 16:33:20 +0200

    ajout du login

--- htdocs/header.php
+++ htdocs/header.php
@@ -1,4 +1,6 @@
-<?php require_once('config/connection.php'); ?>
+<?php 
+session_start();
+require_once('config/connection.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,16 +25,18 @@
         <li class="nav-item offset-1">
             <h4> Spot'Isma </h4>
         </li>
-        <li class="nav-item offset-4">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
-                S'inscrire
-            </button>
-        </li>
-        <li class="nav-item ml-5">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalLog">
-                Se connecter
-            </button>
-        </li>
+        <?php 
+            if (!isset($_SESSION['LOGGED_USER'])) {
+                echo ('<li class="nav-item offset-4">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+                    Login
+                </button>
+                </li>');
+            }
+            else {
+                echo ('<li class="nav_item offset-2" ><h4>Vous êtes connectés en tant que ' . $_SESSION['LOGGED_USER'] . '<a href="process/logout.php">Se déconnecter</a></h4></li>');
+            }
+        ?>
     </ul>
 
 <!-- Modal -->
@@ -46,34 +50,44 @@
                         </button>
                     </div>
                     <div class="modal-body">
-                        <form action="signIn.php" method="POST">
+                        <form action="process/signIn.php" method="POST" id="formSign">
                             <label for="name">Pseudo</label>
                             <input type="text" name="name" id="name">
-                            <input type="submit" class="btn btn-primary" value="envoyer">
+                            <input type="submit" id="submit" class="btn btn-primary" value="envoyer">
                         </form>
+                        <div id="answer">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+<script> 
+    let inputSubmit = document.getElementById('submit');
 
+    inputSubmit.addEventListener('click', function(e) {
+        e.preventDefault();
+        let inputName = document.getElementById('name').value;
+        let objet = {
+            'name' : inputName
+        }
+        console.log(objet);
+        fetch('process/signIn.php', {
+                            method: "POST",
+                            body: JSON.stringify(objet)
+                            }
+    )
+        .then(function(response) {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error('Erreur lors de la requête AJAX');
+                    }
+                })
+        .then(function(data) {
+            document.getElementById('formSign').innerHTML = "";
+            document.getElementById('answer').innerHTML = data.name;
+            })
+    });
+</script>
 <!-- Modal -->
-        <div class="modal fade" id="modalLog" tabindex="-1" role="dialog" aria-labelledby="modalLog" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLongTitle">Se connecter</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <form action="logIn.php" method="POST">
-                            <label for="name">Pseudo</label>
-                            <input type="text" name="name" id="name">
-                            <input type="submit" class="btn btn-primary" value="envoyer">
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
     </header>
--- htdocs/process/signIn.php
+++ htdocs/process/signIn.php
@@ -1,4 +1,33 @@
 <?php
+session_start();
 include('../config/connection.php');
 
+$data = json_decode(file_get_contents('php://input'), true);
+if (isset($data)) {
+    $name = $data['name'];
+    $query = 'SELECT * from users where name = "' . $name . '"';
+    $userStatement = $baseSpotisma -> prepare ($query);
+    $userStatement -> execute();
+    $user =$userStatement -> fetch(PDO::FETCH_ASSOC);
+
+    $answer = [];
+    if (empty($user)) {
+        $insertStatement = $baseSpotisma -> prepare("INSERT INTO users (name, admin) VALUES (:name, :admin)");
+        $insertStatement -> execute([
+            'name' => $name,
+            'admin' => 0
+        ]);
+        $_SESSION['LOGGED_USER'] = $name;
+        $answer['name'] = 'Votre compte a bien été créé';
+    }
+    else {
+        $_SESSION['LOGGED_USER'] = $name;
+        $answer['name'] = "Vous êtes connectés !";
+    }
+}
+else {
+    $answer['name'] = "Veuillez renseigner un pseudo valide";
+}
+$jsonAnswer = json_encode($answer);
+echo $jsonAnswer;
 ?>
